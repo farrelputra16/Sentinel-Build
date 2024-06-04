ea59a62592b7153ae3676f987c66439a7172e97e
Maulana045 <isadmaulana4@stundents.unnes.ac.id>
2024-06-04 20:16:33 +0700

    push

--- app/Http/Controllers/AdminloginController.php
+++ app/Http/Controllers/AdminloginController.php
@@ -31,36 +31,10 @@ class AdminloginController extends Controller
         // Jika autentikasi gagal
         return back()->withErrors(['login' => 'ID atau password salah']);
     }
-
-    // Menampilkan form login pekerja
-    public function showLoginPekerjaForm()
-    {
-        return view('login_pekerja');
-    }
-
-    // Menangani login pekerja
-    public function loginPekerja(Request $request)
-    {
-        // Validasi input
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'id' => 'required|string|max:255',
-        ]);
-
-        // Logika autentikasi pekerja
-        // Misalnya, jika autentikasi berhasil:
-        if ($request->name == 'pekerja' && $request->id == '12345') {
-            return redirect()->route('login-coy');
-        }
-
-        // Jika autentikasi gagal
-        return back()->withErrors(['login' => 'Nama atau ID salah']);
-    }
-
-    // Menampilkan halaman indeks
     public function index()
     {
         $workers = Pekerja::all();
         return view('index', compact('workers'));
     }
+
 }
